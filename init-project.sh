73f8f2ca1e886912230e0e0db3ca8a1e20f264e6
Jeroen Wouters <jeroenwouters@MacBook-Pro.local>
2020-07-29 12:20:13 +0200

    add frontend repo in init script

--- init-project.sh
+++ init-project.sh
@@ -1,1 +1,2 @@
 git clone ssh://git@gitlab.inuits.io:2224/customers/scouts/werkwinkeldatabank/scouts-wwdb-api.git api
+git clone ssh://git@gitlab.inuits.io:2224/customers/scouts/werkwinkeldatabank/scouts-wwdb-frontend.git frontend
