06a0817d651fb9ee367970f6de18d4c71f92d342
sverholen <sverholen@gmail.com>
2021-11-26 09:57:47 +0100

    Add scouts remotes to init script

--- init-project.sh
+++ init-project.sh
@@ -1,6 +1,12 @@
 git clone ssh://git@gitlab.inuits.io:2224/customers/scouts/werkwinkeldatabank/scouts-wwdb-api.git api
 git clone ssh://git@gitlab.inuits.io:2224/customers/scouts/werkwinkeldatabank/scouts-wwdb-frontend.git frontend
 git clone https://github.com/ricardoamaro/drupal8-docker-app.git drupal8-test
+
+cd api
+git remote add scouts git@github.com:ScoutsGidsenVL/werkwinkeldatabank-backend.git
+cd ../frontend
+git remote add scouts git@github.com:ScoutsGidsenVL/werkwinkeldatabank-frontend.git
+cd ..
 # DRUPAL
 # To get drupal working: After the clone cd drupal8-test
 # ./drupal8_local.sh - This will init the project, drupal modules, database etc but will also start the local server which you can cancel once it's finished
